62c8fcaa650a28092e3ac1cb0b5a07e481341f61
KlingFelix <flxkling@gmail.com>
2021-12-04 00:00:25 +0100

    Modify shell scripts

--- run_simulation_test.sh
+++ run_simulation_test.sh
@@ -9,16 +9,18 @@ cd GeneratedFiles
 
 # Input files for specific energies
 # NUMS=($NUMS $(seq 1 18))
-NUMS=($NUMS $(seq 1 1))
+NUMS=($NUMS $(seq 1))
 for NUM in $NUMS; do
-    FILES=($FILES muons_$(echo $NUM)_left.inp muons_$(echo $NUM)_right.inp muons_$(echo $NUM)_top.inp muons_$(echo $NUM)_bot.inp )
+    FILES=($FILES muons_$(echo $NUM)_left.inp muons_$(echo $NUM)_right.inp muons_$(echo $NUM)_top.inp muons_$(echo $NUM)_bot.inp)
 done
 
+echo $FILES
 # Set number of processes you want to run simultaneously
 MAXCPU=4
 for FILE in $FILES; do
+    echo $FILE
     ((i=i%$MAXCPU)); ((i++==0)) && wait
-    $FLUPRO/flutil/rfluka -N0 -M1 $FILE &
+#    $FLUPRO/flutil/rfluka -N0 -M1 $FILE &
 done
 
 cd ..
