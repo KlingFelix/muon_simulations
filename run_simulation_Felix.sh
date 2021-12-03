e218639ff2a6267352e4308a27f04564a7bf473d
KlingFelix <flxkling@gmail.com>
2021-12-03 23:22:19 +0100

    Create run_simulation_Felix.sh

---
+++ run_simulation_Felix.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+# Set FLUKA directory
+export FLUPRO=/Users/felixkling/work/FLUKA
+
+cd GeneratedFiles
+
+# Get input files for runs
+
+# Input files for specific energies
+# NUMS=($NUMS $(seq 1 18))
+NUMS=($NUMS $(seq 1))
+for NUM in $NUMS; do
+    FILES=($FILES muons_$(echo $NUM)_left.inp muons_$(echo $NUM)_right.inp muons_$(echo $NUM)_top.inp muons_$(echo $NUM)_bot.inp )
+done
+
+# Set number of processes you want to run simultaneously
+MAXCPU=4
+for FILE in $FILES; do
+    ((i=i%$MAXCPU)); ((i++==0)) && wait
+    $FLUPRO/flutil/rfluka -N0 -M1 $FILE &
+done
+
+cd ..
